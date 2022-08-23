4782e558ce735cd56af382e458f64cf02e2087b5
aryafaiq <aryafaiq17@gmail.com>
2022-08-23 13:46:19 +0700

    first commit

---
+++ login.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/login.css">
+    <title>ALFA</title>
+</head>
+<body>
+    
+
+<section class="">
+        <div class="container mt-5">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-12 col-lg-8 col-xl-5">
+                    <div class="card text-black rounded mt-4">
+                        <div class="card-body  bg-custom rounded">
+                            <div class=" ">
+                                <div class="img text-center mt-3">
+                                    <img class="mb-5 rounded" src="img/alfa.png" alt="dubes" width="85%">
+                                </div>
+                                <form action="" method="post" class="">
+                                    <div class="form-outline form-white mb-4">
+                                        <label class="form-label ps-2" for="username"><b>Username</b></label>
+                                        <input type="text" id="username" name="username" class="form-control  form-control-lg cardtest" required autocomplete="off" autofocus style="font-size:16px;" placeholder="Username">
+                                    </div>
+                                    <div class="form-outline form-white mb-5">
+                                        <label class="form-label ps-2" for="password"><b>Password</b></label>
+                                        <input type="password" id="password" name="password" class="form-control form-control-lg cardtest" required autocomplete="off" style="font-size:16px;"placeholder="******">
+                                    </div>
+                                    <div class="d-grid gap-2">
+                                    <button class="btn bcustom text-white cardtest pt-2 pb-2" type="submit" style="border-radius: 10px;" name="login">Login</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
+</body>
+</html>
